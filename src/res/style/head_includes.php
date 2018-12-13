602dcca0bdfcaa743ce5bd87ab771cab08c787ea
ahahn94 <ahahn94@outlook.com>
2018-12-13 16:27:57 +0100

    Add Touch Navigation
    Add touch navigation:
    - Add navigation to next/previous page via swiping
    - Remove navigation buttons
    - Move link to front page to the "Daily Augur" title
    - Make links blend in with the rest of the text
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- src/res/style/head_includes.php
+++ src/res/style/head_includes.php
@@ -2,3 +2,6 @@
 <link rel="stylesheet" href="/res/lib/fontawesome-free-5.5.0-web/css/all.css">
 <!-- Custom CSS-->
 <link rel="stylesheet" href="/res/style/style.css">
+
+<!-- Swiper JavaScript-->
+<script type="text/javascript" src="/res/behaviour/swiping.js"></script>
